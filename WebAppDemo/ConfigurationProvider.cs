fa430ec137d914285fbac84122c267f0f576bdee
Radosław Wąsala <rwasala@qzsolutions.pl>
2019-03-08 10:42:40 +0100

    Adjust template for demo

---
+++ WebAppDemo/ConfigurationProvider.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace WebAppDemo
+{
+    public class ConfigurationProvider
+    {
+        private static readonly IConfiguration Config = new ConfigurationBuilder()
+            .AddJsonFile("appsettings.json", false)
+            .AddEnvironmentVariables()
+            .Build();
+
+        public static string EnvName => Config.GetSection("EnvName").Value;
+    }
+}
---
+++ WebAppDemo/Controllers/MainController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebAppDemo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MainController : ControllerBase
+    {
+        // GET api/main
+        [HttpGet]
+        public ActionResult<string> Get()
+        {
+            return "Hello from " + ConfigurationProvider.EnvName;
+        }
+    }
+}
